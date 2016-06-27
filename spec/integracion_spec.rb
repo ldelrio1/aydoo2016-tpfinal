0f30c6b25fae0670f053c8ebdae22c950d8ad18b
ldelrio1 <lucasdr.lgi@gmail.com>
2016-06-27 03:15:36 -0300

    Refactor en Test Crea Integracion con los casos

---
+++ spec/integracion_spec.rb
@@ -0,0 +1,45 @@
+require 'rspec'
+require_relative '../model/nave'
+require_relative '../model/misil'
+
+describe 'Nave contra Nave' do
+
+  it 'Nave1 choca con Nave2 verifica Nave1' do
+    nave1 = Nave.new
+    nave1.set_vida 200
+    nave2 = Nave.new
+    nave2.set_vida 200
+    nave1.chocar_con(nave2)
+    vida_nave1 = 100
+    expect(nave1.vida).to eq vida_nave1
+    end
+
+  it 'Nave1 choca con Nave2 verifica Nave2' do
+    nave1 = Nave.new
+    nave1.set_vida 200
+    nave2 = Nave.new
+    nave2.set_vida 100
+    nave1.chocar_con(nave2)
+    vida_nave2 = 0
+    expect(nave2.vida).to eq vida_nave2
+  end
+end
+
+describe 'Nave contra Misil' do
+
+  it 'Nave choca con Misil verifica Nave' do
+    nave = Nave.new
+    misil = Misil.new
+    nave.chocar_con(misil)
+    vida_nave = 20
+    expect(nave.vida).to eq vida_nave
+  end
+
+  it 'Nave choca con Misil verifica Misil' do
+    nave = Nave.new
+    misil = Misil.new
+    nave.chocar_con(misil)
+    vida_misil = 0
+    expect(misil.vida).to eq vida_misil
+  end
+end
